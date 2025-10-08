28654d5e96f68f02fd7c094b90e8b7e778f86d9b
NAIL-LIRA <nailson-lira@outlook.com>
2025-10-08 09:47:03 -0300

    desconto com função

--- desconto.js
+++ desconto.js
@@ -5,22 +5,31 @@ let precoPago = precoNormal
 
 
 switch(pagamento){
+    case "dinheiro":
+    case "pix":
     case "debito":
-        precoPago=precoNormal-(precoPago*0.1)
-        console.log("O preço pago foi: ",precoPago)
+        console.log("O preço pago foi: ",aplicarDesconto(precoNormal,pagamento))
         break
-    
-    case "dinheiro","pix":
-        precoPago=precoNormal-(precoPago*0.15)
-        console.log("O preço pago foi: ",precoPago)
-        break
+
     case "credito":
         if (parcela<=2){
         precoPago=precoNormal/parcela
         console.log("O preço pago foi: ",precoPago)
         }else{
-            console.log("O preço pago foi: ", precoPago=(precoNormal+(precoNormal*0.10)/parcela), " dividido em ",parcela, " parcelas")
+            console.log("O preço pago foi: ", precoPago=(precoNormal+(precoNormal*1.10)/parcela), " dividido em ",parcela, " parcelas")
         }
 
 }
 
+function aplicarDesconto(precoNormal,pagamento){
+    switch(pagamento){
+        case "debito":
+            return precoNormal-(precoNormal*0.1)
+            
+        case "dinheiro":
+        case "pix":
+            return precoNormal-(precoNormal*0.15)
+        default:
+            return precoNormal
+    }
+}
